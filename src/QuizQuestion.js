06ffd50894882670ba87806bea51fc4c05a3bc4e
Michael Egner <michael@theventuresmiths.com>
2018-12-17 18:20:03 +0200

    Mapped answer options to the button component to show the quiz options

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -9,7 +9,7 @@ class QuizQuestion extends Component {
             </section>
             <section className="buttons">
                 <ul>
-                    <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+                    {this.props.quiz_question.answer_options.map( (value, index) => <QuizQuestionButton key={index} button_text={value} /> )}
                 </ul>
             </section>
         </main>
