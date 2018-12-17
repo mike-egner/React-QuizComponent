5e924f382ca09154b1fbf4f087e25acc1442bfbc
Michael Egner <michael@theventuresmiths.com>
2018-12-17 15:06:58 +0200

    passed quiz data as props to quizquestion and render contents

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component {
         this.state = {quiz_position: 1};
     }
     render () {
-        return <QuizQuestion />
+        return <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
     }
 }
 
